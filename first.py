bf33028b3d17ae93cb03fdece7ec6b68c7b40f5f
Aniket Kangude <142888993+Aniket0727@users.noreply.github.com>
2024-09-13 00:24:03 +0530

    Add files via upload

---
+++ first.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+
+
+x=['anu','om','yash','rahul','ashu']
+y=[80,90,80,60,40]
+color=['red','pink','yellow','blue','gray']
+plt.bar(x,y,color=color)
+plt.xlabel('Names')
+plt.ylabel('makrs')
+plt.title("Ranking")
+
+plt.show()
+print("first")
+print("second")
